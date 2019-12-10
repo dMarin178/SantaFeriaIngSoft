1027c68efee403700ef66deb0539145d5229323d
Dino Marin <dinomarindiaz178gmail.com>
2019-12-09 21:17:20 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-agregar').onclick = () => { agregarProducto() }
+
+function agregarProducto() {
+    let nombre = document.getElementById('inputNombre').value;
+
+    alert( nombre )
+}
+
+
